fad55914395691ff672477740e2a3ab83de0d353
Rhilip <rhilipruan@gmail.com>
2020-08-29 09:01:38 +0800

    fix

--- U2TrackerUpdater.py
+++ U2TrackerUpdater.py
@@ -21,9 +21,30 @@ import os
 import time
 
 import requests
-import qbittorrent
 import transmissionrpc
 from deluge_client import DelugeRPCClient
+from qbittorrent import Client as QbittorrentClient
+
+# 声明
+notes = """
+# U2TrackerUpdater-1.5
+# 批量更新任务Tracker地址中的秘钥
+# 原作者：U2@杯杯杯杯具(https://gist.github.com/tongyifan/83220b417cffdd23528860ee0c518d15)
+# Tr-U2@ITGR(https://gist.github.com/inertia42/f6120d118e47925095dbceb5e8e27272)
+# De-U2@種崎敦美(https://github.com/XSky123/dmhy_change_securekey_deluge)
+# 整合优化：U2@Loid(https://github.com/LoidVC/U2TrackerUpdater)
+# 感谢帮助：U2@vincent-163(PR#1),U2@x琳x(PR#3),U2@Noira(论坛#139477)
+
+# 原作者备注
+# 0. 免责声明：程序仅在本地客户端qBittorrent v4.2.5/Transmission v2.94/Deluge v1.3.15上测试通过，运行结果与作者无关
+# 1. 已知bug：从第 48 个请求开始会连续失败 10 次，在管理组修复之前请手动重复执行至所有种子更新完毕，直到显示找到0个未被更新的种子为止
+
+# 优化备注
+# 1.添加交互逻辑
+# 2.整合多客户端
+# 3.本工具仅限用于于个人更新Tracker中的秘钥，禁止利用其进行带宽和运算资源占用、数据挖掘、规律遍历、商业使用或类似的活动。违规操作造成的警告、禁用相关账户，封锁IP、中止或终止API等后果自负责任。
+"""
+
 
 # 和U2的jsonrpc交互部分
 
@@ -37,9 +58,19 @@ class BtClient(object):
     cache_file = "updated_torrents.json"
 
     apikey = None
+    cached_hashes = []
 
     def __init__(self):
         self.apikey = input("输入APIKEY（站内查看API地址中的apikey=后面的部分）：")
+
+        # 加载缓存
+        if os.path.exists(self.cache_file):
+            with open(self.cache_file, "r") as fp:
+                self.cached_hashes = json.load(fp)
+
+    def saveCachedTorrentHashes(self):
+        with open(self.cache_file, "w") as fp:
+            json.dump(self.cached_hashes, fp)
 
     def getAllTorrentHashes(self) -> list:
         """
@@ -49,20 +80,9 @@ class BtClient(object):
         """
         raise NotImplementedError()
 
-    def getCachedTorrentHashes(self) -> list:
-        if not os.path.exists(self.cache_file):
-            return []
-        else:
-            with open(self.cache_file, "r") as fp:
-                return json.load(fp)
-
-    def setCachedTorrentHashes(self, updated_torrents):
-        with open(self.cache_file, "w") as fp:
-            json.dump(self.getCachedTorrentHashes() + updated_torrents, fp)
-
     def getUnCachedTorrentHashes(self) -> list:
         return [
-            t for t in self.getAllTorrentHashes() if t["hash"] not in self.getCachedTorrentHashes()
+            t for t in self.getAllTorrentHashes() if t["hash"] not in self.cached_hashes
         ]
 
     def changeTorrentTracker(self, info, item):
@@ -79,13 +99,12 @@ class BtClient(object):
 
     def runJob(self):
         u2_torrent_info_hash = self.getUnCachedTorrentHashes()
-        print(f"找到了{len(u2_torrent_info_hash)}个未被更新的种子～")
+        print("找到了{}个未被更新的种子～".format(len(u2_torrent_info_hash)))
 
         # 防止单次提交的info_hashes过多的情况，把他chunk成{batch_size}个作为一组提交
-        updated_torrents = []
         for i in range(0, len(u2_torrent_info_hash), self.batch_size):
             deal_torrents = u2_torrent_info_hash[i:i + self.batch_size]
-            print(f"正在获取第{i}到第{i + len(deal_torrents)}个种子的新secret key")
+            print("正在获取第{}到第{}个种子的新secret key".format(i, i + len(deal_torrents)))
 
             request_data = []
             _index = {}
@@ -104,7 +123,7 @@ class BtClient(object):
             if resp.status_code == 503:
                 while resp.status_code == 503:
                     wait_second = int(resp.headers["Retry-After"]) + 5
-                    print(f"速度过快，将在{wait_second}秒后重试")
+                    print("速度过快，将在{}秒后重试".format(wait_second))
                     time.sleep(wait_second)
                     resp = requests.post(
                         endpoint, params={"apikey": self.apikey}, json=request_data
@@ -115,7 +134,7 @@ class BtClient(object):
                 exit()
 
             if resp.status_code != 200:
-                print(f"意外的错误：{resp.status_code}")
+                print("意外的错误：{}".format(resp.status_code))
 
             response_data = resp.json()
             for item in response_data:
@@ -127,9 +146,10 @@ class BtClient(object):
 
                 print("正在编辑种子 {} 的Tracker信息".format(_index[item["id"]]["hash"]))
                 self.changeTorrentTracker(_index[item["id"]], item)
-                updated_torrents.append(_index[item["id"]]["hash"])
+                self.cached_hashes.append(_index[item["id"]]["hash"])
 
-        self.setCachedTorrentHashes(updated_torrents)
+            # 每个轮次结束后就更新缓存，不用等到所有结束后再缓存
+            self.saveCachedTorrentHashes()
 
 
 class QBittorrent(BtClient):
@@ -139,7 +159,7 @@ class QBittorrent(BtClient):
         self.user = input("输入客户端用户名：")
         self.password = input("输入客户端密码：")
 
-        qb = qbittorrent.Client(self.host)
+        qb = QbittorrentClient(self.host)
         print("开始连接 Qbittorrent 客户端...")
         qb.login(self.user, self.password)
         self.qb = qb
@@ -149,10 +169,16 @@ class QBittorrent(BtClient):
 
         u2_torrent_info_hash = []
         for torrent in torrents:
-            if any([tracker in torrent.get("tracker", "") for tracker in u2_tracker]):
-                u2_torrent_info_hash.append(
-                    {"hash": torrent.get("hash"), "tracker": torrent.get("tracker")}
-                )
+            hashes = torrent["hash"]
+            trackers = self.qb.get_torrent_trackers(hashes)
+
+            for tker in trackers:
+                if any([tracker in tker["url"] for tracker in u2_tracker]):
+                    u2_torrent_info_hash.append(
+                        {"hash": hashes, "tracker": tker["url"]}
+                    )
+                    continue
+
         return u2_torrent_info_hash
 
     def changeTorrentTracker(self, info, item):
@@ -223,9 +249,7 @@ class Deluge(BtClient):
 
 if __name__ == '__main__':
     # 声明
-    print("# U2TrackerUpdater\n# 批量更新任务Tracker地址中的秘钥\n# 原作者：U2@杯杯杯杯具(https://gist.github.com/tongyifan/83220b417cffdd23528860ee0c518d15)\n# Tr-U2@ITGR(https://gist.github.com/inertia42/f6120d118e47925095dbceb5e8e27272)\n# De-U2@種崎敦美(https://github.com/XSky123/dmhy_change_securekey_deluge)\n# 整合优化：U2@Loid(https://github.com/LoidVC/U2TrackerUpdater)")
-    print('# 原作者备注\n# 0. 免责声明：程序仅在本地客户端qBittorrent v4.2.5/Transmission v2.94/Deluge v1.3.15上测试通过，运行结果与作者无关\n# 1. 已知bug：从第 48 个请求开始会连续失败 10 次，在管理组修复之前请手动重复执行至所有种子更新完毕，直到显示找到0个未被更新的种子为止')
-    print("# 优化备注\n# 1.添加交互逻辑\n# 2.整合多客户端\n# 3.本工具仅限用于于个人更新Tracker中的秘钥，禁止利用其进行带宽和运算资源占用、数据挖掘、规律遍历、商业使用或类似的活动。违规操作造成的警告、禁用相关账户，封锁IP、中止或终止API等后果自负责任。")
+    print(notes)
     termDes = input('# 我已阅读并同意上述条款（Y/N）：')
     if termDes.lower() != 'y':
         exit()
